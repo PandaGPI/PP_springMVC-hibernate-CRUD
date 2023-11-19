e9057dd85177c435b2a1b86ce86e48d686443299
PandaGPI <pirograg@gmail.com>
2023-11-19 14:10:45 +0300

    TASK003: 1. Рбота над ошибками

--- src/main/java/ru/pirogov/web/AppInit.java
+++ src/main/java/ru/pirogov/web/AppInit.java
@@ -5,16 +5,19 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 import ru.pirogov.web.config.WebConfig;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
     }
 
+    @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{
                 WebConfig.class
         };
     }
 
+    @Override
     protected String[] getServletMappings() {
         return new String[]{
                 "/"
